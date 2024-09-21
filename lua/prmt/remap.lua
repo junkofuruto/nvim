df64f864101e5008d8985394fba99ddd04297cc3
Theodore Kaczynski <ah3082340@gmail.com>
2024-09-21 21:16:51 +0300

    addition: window manupulation + qol

--- lua/prmt/remap.lua
+++ lua/prmt/remap.lua
@@ -1,5 +1,15 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex);
+vim.keymap.set('n', '<leader>wc', ':quit<CR>');
+vim.keymap.set('n', '<leader>wsh', ':split<CR>');
+vim.keymap.set('n', '<leader>wsv', ':vsplit<CR>');
+vim.keymap.set('n', '<leader>wl', '<C-w>h');
+vim.keymap.set('n', '<leader>wu', '<C-w>j');
+vim.keymap.set('n', '<leader>wd', '<C-w>k');
+vim.keymap.set('n', '<leader>wr', '<C-w>l');
+
+vim.keymap.set('i', '<C-d>', '<Esc>YpA');
+vim.keymap.set('i', '<C-z>', '<Esc>uA');
 
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
