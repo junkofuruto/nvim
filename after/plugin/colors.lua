0d70c5d6a00565fb42ce887770ddb9e625d4aa61
Theodore Kaczynski <ah3082340@gmail.com>
2024-12-25 12:52:29 +0300

    fix: line number coloration

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,3 +1,6 @@
 vim.cmd.colorscheme('moonfly')
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
