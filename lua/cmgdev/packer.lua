1263e2cfb7ad83a730a62bc3eafb33d3d25d9bc5
Theodore Kaczynski <ah3082340@gmail.com>
2025-09-25 04:40:48 +0300

    update: idk what changed at this point

--- lua/cmgdev/packer.lua
+++ lua/cmgdev/packer.lua
@@ -1,12 +1,14 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require("packer").startup(function(use)
+    use { "Shatur/neovim-ayu", config = function() require("cmgdev.config.colortheme") end }
+
     use { "mason-org/mason.nvim" }
 	use { "nvim-lua/plenary.nvim" }
     use { "neovim/nvim-lspconfig" }
     use { "wbthomason/packer.nvim" }
+    use { "nvim-treesitter/nvim-treesitter-textobjects" }
     use { "nvim-telescope/telescope.nvim", tag = "0.1.8" }
-    use { "Shatur/neovim-ayu", config = function() require("cmgdev.config.ayu") end }
     use { "f-person/git-blame.nvim", config = function() require("cmgdev.config.git-blame") end }
     use { "lewis6991/gitsigns.nvim", config = function() require("cmgdev.config.git-signs") end }
     use { "nvim-lualine/lualine.nvim", config = function() require("cmgdev.config.lualine") end }
