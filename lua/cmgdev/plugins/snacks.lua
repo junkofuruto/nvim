48f9e8b07e9127c40ce33d9d6f608930ad0292be
Theodore Kaczynski <ah3082340@gmail.com>
2025-09-28 20:43:27 +0300

    update: removed dashboard

--- lua/cmgdev/plugins/snacks.lua
+++ lua/cmgdev/plugins/snacks.lua
@@ -27,18 +27,6 @@ return {
                 cycle = false
             }
         },
-        dashboard = {
-            enabled = true,
-            sections = {
-                {
-                    section = "terminal",
-                    cmd = "aic " .. os.getenv("HOME") .. "/.config/nvim/assets/header.jpg -C -c -d 75,50",
-                    height = 50,
-                    width = 75,
-                    random = false,
-                }
-            }
-        }
     },
     keys = {
         { "<leader>gl", function() require("snacks").lazygit.log() end,                      desc = "[CMGDEV] Git Log" },
