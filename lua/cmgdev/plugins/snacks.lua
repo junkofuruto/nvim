a1f4bdf33adeeabdca3956927dd11ce9df87869d
Theodore Kaczynski <ah3082340@gmail.com>
2025-09-29 05:50:06 +0300

    update: pf shows hidden files

--- lua/cmgdev/plugins/snacks.lua
+++ lua/cmgdev/plugins/snacks.lua
@@ -32,7 +32,7 @@ return {
         { "<leader>gl", function() require("snacks").lazygit.log() end,                      desc = "[CMGDEV] Git Log" },
         { "<leader>gs", function() require("snacks").picker.git_status() end,                desc = "[CMGDEV] Git Status" },
         { "<leader>gb", function() require("snacks").picker.git_branches() end,              desc = "[CMGDEV] Select and Switch Git Branches" },
-        { "<leader>pf", function() require("snacks").picker.files() end,                     desc = "[CMGDEV] Treesitter Find File" },
+        { "<leader>pf", function() require("snacks").picker.files({ hidden = true }) end,    desc = "[CMGDEV] Treesitter Find File" },
         { "<leader>pg", function() require("snacks").picker.grep() end,                      desc = "[CMGDEV] Grep Word" },
         { "<leader>pt", function() require("snacks").picker.todo_comments() end,             desc = "[CMGDEV] Find all TODO statements" },
         { "<leader>km", function() require("snacks").picker.keymaps({ layout = "ivy" }) end, desc = "[CMGDEV] Search Keymaps" }
